075cf2c8574737b419d0d26c0a76c34b5b6547b7
ᦖєgιт <support@legitsoulja.info>
2017-07-24 21:07:54 -0400

    Create MySQL.php

---
+++ lib/SQLObject/Adapter/MySQL.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SlickInject\SQLObject\Adapter;
+
+use \SlickInject\SQLObject\SQLInterface as SQLInterface;
+
+class MySQL implements SQLInterface {
+  private static $con;
+  private $defaultDB;
+  
+  function __construct(){}
+  public function connect($dbhost, $dbuser, $dbpass, $dbname){}
+  public function close(){}
+  public function selectDB($name){}
+  public function setDefaultDB(){}
+  public function getErrorCode(){}
+  public function getError(){}
+  public function query($query, $bind_args = NULL){}
+  
+}
